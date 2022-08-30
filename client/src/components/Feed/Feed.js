9072deb10f049888f1402c10a93bc1a933c5bf23
hemantsharma1498 <hemantsharma1498@gmail.com>
2022-08-30 14:21:00 +0530

    done

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -3,11 +3,21 @@ import React from 'react'
 function Feed() {
   return (
     <section className="flex justify-center h-[788px] w-full">
+      {/* FAVOURITE TOPICS */}
+      <div className="flex justify-center w-1/6 pt-10">
+        <p className="text-black">
+          Favourites:
+        </p>
+      </div>
+      
+      {/* MAIN NEWS GRID */}
       <div className="bg-white flex justify-center w-4/6  grid-cols-3">
         <p className="text-3xl">
           SOME PURE ABSOLUTE BULLSHIT
         </p>  
       </div>
+
+      <div className="w-1/6"></div>
       
       </section>
   )
