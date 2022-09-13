c1c2de6ecdd75d8a42b6f02701059f8fb123bcb2
Hemant <hemantsharma1498@gmail.com>
2022-09-13 20:25:51 +0530

    minor changes

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import sidebar from '../secondaryComponents/sidebar'
 
 function Feed(props) {
   return (
     <section className="flex justify-center h-[788px] w-full">
       {/* FAVOURITE TOPICS */}
       <div className="flex justify-center w-1/6 pt-10">
-      
-        <sidebar className="text-black h-10 bg-lmgray px-4 pt-1 border-2 border-gray rounded-md"/>
       </div>
       
       {/* MAIN NEWS GRID */}
       <div className="bg-white flex justify-center w-4/6  grid-cols-3">
+        {/*TODO: DYNAMIC FEED FUNCTONALITY*/}
         <p className="text-3xl pt-3">
           SOME PURE ABSOLUTE 
         </p>  
